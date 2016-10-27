a9453fc16225929541db5585d757504b90255e5f
Alex Ward <alex@volcaniccreations.com>
2016-10-27 18:42:46 +0100

    ejs templates now work with json and use dynamic routes, I still need to add options for invalid routes and then I can start working on styling and content

--- src/routes/loader.js
+++ src/routes/loader.js
@@ -1,17 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var pages = require('../json/pages.json');
 
 router.get('/', (req, res) => {
     res.render(
       'index',
       {
-        title: 'Welcome to Go-Compare'
+        title: 'Welcome'
       }
     )
-})
+});
+
+router.get('/:slug', (req, res) => {
+    var slug = req.params.slug;
+    pages.forEach(function(page) {
+      if(page.slug === slug) {
+        res.render(
+          'article', page
+        )
+      }
+    });
+});
 
 router.get('*', (req, res) => {
-  res.send('404 not found')
-})
+  res.send('<h1>404 not found</h1>');
+});
 
 module.exports = router;
